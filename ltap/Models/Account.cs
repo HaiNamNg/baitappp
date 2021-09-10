91f26b39e59a87b410b9387d6459078843aad827
NGUYEN HAI NAM <sepnamdz@gmail.com>
2021-09-10 14:40:12 +0700

    1

--- ltap/Models/Account.cs
+++ ltap/Models/Account.cs
@@ -5,7 +5,20 @@ using System.Web;
 
 namespace ltap.Models
 {
-    public class Account
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Account")]
+    public partial class Account
     {
+        [Key]
+        [StringLength(50)]
+        public string Usename { get; set; }
+
+        [StringLength(50)]
+        public string Password { get; set; }
     }
 }
