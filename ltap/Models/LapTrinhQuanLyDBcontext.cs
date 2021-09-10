5e835b1907a9104bb925b500a23c985606960fa6
NGUYEN HAI NAM <sepnamdz@gmail.com>
2021-09-10 14:07:20 +0700

    Add project files.

---
+++ ltap/Models/LapTrinhQuanLyDBcontext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ltap.Models
+{
+    public class LapTrinhQuanLyDBcontext: DbContext
+    {
+        public LapTrinhQuanLyDBcontext() : base("LapTrinhQuanLyDBcontext")
+        {
+        }
+
+        public virtual<acc> Students { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<SinhVien> SinhViens { get; set; }
+
+    }
+}
+//DESKTOP-GIPHEE4\SQLEXPRESS
